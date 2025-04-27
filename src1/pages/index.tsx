3271344fcefbfd703cedf2dd1d56b3b26e11799e
Rick Dutta <144031365+rick-dutta@users.noreply.github.com>
2025-04-27 13:58:05 +0530

    Create index.tsx

---
+++ src1/pages/index.tsx
@@ -0,0 +1,18 @@
+
+import Hero from "@/components/Hero";
+import Skills from "@/components/Skills";
+import Experience from "@/components/Experience";
+import Education from "@/components/Education";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-[#1A1F2C]">
+      <Hero />
+      <Skills />
+      <Experience />
+      <Education />
+    </div>
+  );
+};
+
+export default Index;
